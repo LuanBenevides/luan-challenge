3604ed90360f4ccea0e2a12d8bcd0de9e1f5e1d9
Luan Benevides <66485746+LuanBenevides@users.noreply.github.com>
2022-03-08 21:57:29 -0300

    MVP v1.2 (sign up, login e logout - ok)

--- scripts/validPass.js
+++ scripts/validPass.js
@@ -17,6 +17,15 @@ let usuarios = [
         password: "Senha@789"
     }
 ]
+let userName = document.getElementById("userName");
+let userMail = document.getElementById("userMail");
+let userNickName = document.getElementById("userNickName");
+let userPassword = document.getElementById("userPassword");
+let confirmUserPassword = document.getElementById("confirmUserPassword");
+let abrir = document.getElementById("createAccount");
+let botaoDeCriarConta = document.getElementById("newAccount");
+let cancelCNACC= document.getElementById("cancelNewAccountCreate");
+
 
 function startLogon(){
     let userLogin = document.getElementById("userLogin").value;
@@ -24,15 +33,67 @@ function startLogon(){
 
     for(controle =0; controle < usuarios.length;controle++){
         if(userLogin == usuarios[controle].name && userPassLogin == usuarios[controle].password){
-            alert("Usuário logado com sucesso!");
-            controle = usuarios.length + 1;
+            openSistema();
+            controle = usuarios.length + 2;
         }
     }if(controle == usuarios.length){
         alert("Usuário não cadastrado!");
         controle = 0;
     }
 }
+function openCloseNewAccountArea(){
+    if(abrir.classList.contains("createAccount")){
+        abrir.classList.remove("createAccount");
+        abrir.classList.add("createAccountActivate");
+    }else if(abrir.classList.contains("createAccountActivate")){
+        abrir.classList.remove("createAccountActivate");
+        abrir.classList.add("createAccount");
+    }
+}
+
+function cadastroSolicitado(){
+    let nome1 = userName.value;
+    let email1 = userMail.value;
+    let senha1 = userPassword.value;
+    let confirmaSenha1 = confirmUserPassword.value;
+    let nickname1 = userNickName.value;
+    
+    senha1.toString();
+    confirmaSenha1.toString();
+
+    if(nome1.value == null && email1 == null && nickname1 == null && senha1 == null && confirmaSenha1 == null){
+        alert("Dados vazios! Por favor, preencha o formulário...");
+    }else if(senha1.length < 6 ){
+        alert("Senha menor que 6 caracteres, por favor inserir uma senha forte e que contenha mais de 6 caracteres!");
+    }else if(senha1 != confirmaSenha1){
+        alert("As senhas não conferem!");
+    }else{
+        usuarios.push(
+            {
+                name: nickname1,
+                email: email1,
+                password: senha1
+            }
+        );
+        alert("Usuário cadastrado! Indo para a tela de login...");
+        openCloseNewAccountArea();
+        console.log(usuarios);
+    }
+}
+
+function openSistema(){
+    let home = document.getElementById("homePage");
+    if(home.classList.contains("homePage")){
+        home.classList.remove("homePage");
+        home.classList.add("homePageActivate");
+    }else if(home.classList.contains("homePageActivate")){
+        home.classList.remove("homePageActivate");
+        home.classList.add("homePage");
+    }
+}
 
 
+botaoDeCriarConta.addEventListener("click", openCloseNewAccountArea);
+cancelCNACC.addEventListener("click", openCloseNewAccountArea);
 
 
